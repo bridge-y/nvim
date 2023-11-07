0938c32d04378d0a9e9700d2575460ae7cdaa174
bridgey <bridgey4461@protonmail.ch>
2023-11-07 20:55:56 +0900

    feat(nvim-ts-autotag): use nvim-ts-autotag

--- lua/modules/lang/package.lua
+++ lua/modules/lang/package.lua
@@ -35,3 +35,35 @@ package({
   end,
 })
 
+package({
+  'windwp/nvim-ts-autotag',
+  ft = {
+    'astro',
+    'glimmer',
+    'handlebars',
+    'html',
+    'javascript',
+    'javascriptreact',
+    'jsx',
+    'markdown',
+    'php',
+    'rescript',
+    'svelte',
+    'tsx',
+    'typescript',
+    'typescriptreact',
+    'vue',
+    'xml',
+  },
+  config = function()
+    require('nvim-ts-autotag').setup()
+    vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+      underline = true,
+      virtual_text = {
+        spacing = 5,
+        severity_limit = 'Warning',
+      },
+      update_in_insert = true,
+    })
+  end,
+})
