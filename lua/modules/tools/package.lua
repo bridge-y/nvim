c93d20285aa4db17906ad2e38970ecbb46435e84
bridgey <bridgey4461@protonmail.ch>
2025-01-14 22:48:48 +0900

    feat(image.nvim): add new plugin

--- lua/modules/tools/package.lua
+++ lua/modules/tools/package.lua
@@ -129,3 +129,14 @@ package({
   },
 })
 
+package({
+  {
+    '3rd/image.nvim',
+    opts = {},
+  },
+}, {
+  rocks = {
+    hererocks = true, -- recommended if you do not have global installation of Lua 5.1.
+  },
+})
+
