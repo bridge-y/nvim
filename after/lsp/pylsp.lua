2df71310e13aaed213ac1253260a190139ef0a8d
bridgey <bridgey4461@protonmail.ch>
2026-01-11 22:25:52 +0900

    feat(nvim-lspconfig): feat(lsp): update LSP configuration for Neovim v0.11

---
+++ after/lsp/pylsp.lua
@@ -0,0 +1,35 @@
+return {
+  settings = {
+    pylsp = {
+      plugins = {
+        autopep8 = {
+          enabled = 0,
+        },
+        black = {
+          enabled = 1,
+        },
+        flake8 = {
+          enabled = 1,
+        },
+        mccabe = {
+          enabled = 0,
+        },
+        pycodestyle = {
+          enabled = 0,
+        },
+        pyflakes = {
+          enabled = 0,
+        },
+        pyls_isort = {
+          enabled = 1,
+        },
+        isort = {
+          enabled = 1,
+        },
+        flake8_isort = {
+          enabled = 1,
+        },
+      },
+    },
+  },
+}
---
+++ after/lsp/pyright.lua
@@ -0,0 +1,13 @@
+return {
+  settings = {
+    python = {
+      venvPath = '.',
+      pythonPath = '.venv/bin/python',
+      analysis = {
+        autoSearchPaths = true,
+        diagnosticMode = 'workspace',
+        useLibraryCodeForTypes = true,
+      },
+    },
+  },
+}
